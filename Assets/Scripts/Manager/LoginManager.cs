b7d6d62a4c8b5aff84bff5f68cf13289fae5dc24
ponlawat <ponlawat30109@users.noreply.github.com>
2025-09-25 19:32:44 +0700

    Update scripts

--- Assets/Scripts/Manager/LoginManager.cs
+++ Assets/Scripts/Manager/LoginManager.cs
@@ -5,8 +5,6 @@ using System.Collections;
 public class LoginManager : Singleton<LoginManager>
 {
     public string Username { get; set; }
-    // public UserData CurrentUser { get; set; }
-    // public InventoryData[] Inventory { get; set; }
 
     public IEnumerator Login(string username)
     {
@@ -36,43 +34,4 @@ public class LoginManager : Singleton<LoginManager>
             }
         }
     }
-
-    // public IEnumerator RefreshInventory()
-    // {
-    //     if (CurrentUser == null) yield break;
-    //     string url = $"http://localhost:3000/login/refresh?username={CurrentUser.name}";
-    //     string url = $"http://172.236.153.97:3000/login/refresh?username={CurrentUser.name}";
-    //     using UnityWebRequest www = UnityWebRequest.Get(url);
-    //     yield return www.SendWebRequest();
-    //     if (www.result == UnityWebRequest.Result.Success)
-    //     {
-    //         var response = JsonUtility.FromJson<LoginResponse>(www.downloadHandler.text);
-    //         if (response != null && response.inventory != null)
-    //         {
-    //             Inventory = response.inventory;
-    //         }
-    //     }
-    // }
 }
-
-// [System.Serializable]
-// public class UserData
-// {
-//     public string name;
-//     public int gacha_remaining;
-// }
-
-// [System.Serializable]
-// public class InventoryData
-// {
-//     public int item_id;
-//     public int quantity;
-// }
-
-// [System.Serializable]
-// public class LoginResponse
-// {
-//     public bool success;
-//     public UserData user;
-//     public InventoryData[] inventory;
-// }
